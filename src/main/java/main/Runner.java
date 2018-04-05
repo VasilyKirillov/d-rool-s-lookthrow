75fc40e207e191a21c943050475aae9ac4593d12
Kirillov Vasilii <vkirillov@bmail.ru>
2018-04-05 14:58:11 +0400

    drools tutorial. step 4. stateful session. fireAllRules to execute all rules

--- src/main/java/main/Runner.java
+++ src/main/java/main/Runner.java
@@ -1,36 +1,31 @@
 package main;
 
-
-import model.Person;
+import model.Room;
+import model.Sprinkler;
 import org.kie.api.KieServices;
-import org.kie.api.command.Command;
-
-import org.kie.api.command.KieCommands;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
-import org.kie.api.runtime.StatelessKieSession;
 
-
-import java.util.ArrayList;
-
-import java.util.List;
-
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Runner {
     public static void main(String[] args) {
         KieServices kieServices = KieServices.get();
         KieContainer kieContainer = kieServices.getKieClasspathContainer();
         KieSession kieSession = kieContainer.newKieSession();
-        KieCommands kieCommands = kieServices.getCommands();
-//doesn't work batch command
-        List<Command> cmds = new ArrayList<Command>();
-        cmds.add( kieCommands.newInsert( new Person( "Mr John Smith" ), "mrSmith", true, null ) );
-        cmds.add( kieCommands.newInsert( new Person( "Mr John Doe" ), "mrDoe", true, null ) );
-        BatchExecutionResults results = kieSession.execute( kieCommands.newBatchExecution( cmds ) );
-        assertEquals( new Person( "Mr John Smith" ), results.getValue( "mrSmith" ) );
 
+        String[] names = new String[]{"kitchen", "bedroom", "office", "livingroom"};
+        Map<String, Room> name2room = new HashMap<>();
+        for( String name: names ){
+            Room room = new Room( name );
+            name2room.put( name, room );
+            kieSession.insert( room );
+            Sprinkler sprinkler = new Sprinkler( room );
+            kieSession.insert( sprinkler );
+        }
+
+        kieSession.fireAllRules();
     }
 
 }
