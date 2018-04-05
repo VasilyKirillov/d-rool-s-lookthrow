ec40cd9dfccbc022af3c5a0b4416ac44a320a0a7
Kirillov Vasilii <vkirillov@bmail.ru>
2018-04-05 14:46:02 +0400

    drools tutorial. step 3. batch procesing. stateless. not working example

--- src/main/java/main/Runner.java
+++ src/main/java/main/Runner.java
@@ -1,27 +1,35 @@
 package main;
 
-import model.Applicant;
-import model.Application;
+
+import model.Person;
 import org.kie.api.KieServices;
+import org.kie.api.command.Command;
+
+import org.kie.api.command.KieCommands;
 import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
 import org.kie.api.runtime.StatelessKieSession;
 
-import java.util.Arrays;
+
+import java.util.ArrayList;
+
+import java.util.List;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class Runner {
     public static void main(String[] args) {
-        KieServices kservices = KieServices.get();
-        KieContainer kieContainer = kservices.getKieClasspathContainer();
-
-        StatelessKieSession kSession = kieContainer.newStatelessKieSession();
-        Applicant applicant = new Applicant( "Mr John Smith", 17 );
-        Application application = new Application();
-        assertTrue( application.isValid() );
-        kSession.execute( Arrays.asList( new Object[] { application, applicant } ) );
-        assertFalse( application.isValid() );
+        KieServices kieServices = KieServices.get();
+        KieContainer kieContainer = kieServices.getKieClasspathContainer();
+        KieSession kieSession = kieContainer.newKieSession();
+        KieCommands kieCommands = kieServices.getCommands();
+//doesn't work batch command
+        List<Command> cmds = new ArrayList<Command>();
+        cmds.add( kieCommands.newInsert( new Person( "Mr John Smith" ), "mrSmith", true, null ) );
+        cmds.add( kieCommands.newInsert( new Person( "Mr John Doe" ), "mrDoe", true, null ) );
+        BatchExecutionResults results = kieSession.execute( kieCommands.newBatchExecution( cmds ) );
+        assertEquals( new Person( "Mr John Smith" ), results.getValue( "mrSmith" ) );
 
     }
 
